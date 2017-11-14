1d2d284940bbe34971acf8f755c0ee7ad71ba708
Bryan Forbes <bryan@reigndropsfall.net>
2017-11-14 15:29:16 -0600

    Update deps, bump minor version, and force intern to use its own @dojo/interfaces with global-style=true

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Command, Helper, OptionsHelper } from '@dojo/cli/interfaces';
+import { Command, Helper, OptionsHelper } from '@dojo/interfaces/cli';
 import { join } from 'path';
 import exportProject from './exportProject';
 const pkgDir = require('pkg-dir');
